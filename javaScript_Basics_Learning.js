8a21a86b62625c9846ab03c4c7be586438053277
sid <sidlamkhade@gmail.com>
2024-09-12 16:51:55 +0530

    add renamed files

--- javaScript_Basics_Learning.js
+++ javaScript_Basics_Learning.js
@@ -2,8 +2,28 @@ let k;
 k=34;
 console.log(k);
 
+let g=23;
+g='amar';  //Dynamic language
+console.log(g);
 //Variables are declared using let keyword
 
-let a=23;
-a='amar';  //Dynamic language
-console.log(a);
+//............🔴🟠🟡JavaScript Projects Faulty Calculator..........
+let random = Math.random();
+let a= prompt("Enter first number");
+let b=prompt("Enter second number");
+let c=prompt ("Enter Operation");
+let object={ 
+    "+":"-",
+    "*":"+",
+    "-":"/",
+    "/":"**",
+}
+if(random>0.1)
+{
+    //perform correct calculation
+}
+else
+{
+    //perform wrong calculation
+    c=object[c];
+}
