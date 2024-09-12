839746cee84d5b383257e221fa640d4ff5b495c9
sid <sidlamkhade@gmail.com>
2024-09-12 18:12:38 +0530

    adding learningJavaScriptHTML2.html

--- javaScript_Basics_Learning.js
+++ javaScript_Basics_Learning.js
@@ -1,29 +1,31 @@
 let k;
-k=34;
+k = 34;
 console.log(k);
 
-let g=23;
-g='amar';  //Dynamic language
+let g = 23;
+g = 'amar';  // Dynamic language
 console.log(g);
-//Variables are declared using let keyword
 
-//............🔴🟠🟡JavaScript Projects Faulty Calculator..........
+// Variables are declared using let keyword
+
+// ............🔴🟠🟡JavaScript Projects Faulty Calculator..........
 let random = Math.random();
-let a= prompt("Enter first number");
-let b=prompt("Enter second number");
-let c=prompt ("Enter Operation");
-let object={ 
-    "+":"-",
-    "*":"+",
-    "-":"/",
-    "/":"**",
-}
-if(random>0.1)
-{
-    //perform correct calculation
-}
-else
-{
-    //perform wrong calculation
-    c=object[c];
+let a = prompt("Enter first number");
+let b = prompt("Enter second number");
+let c = prompt("Enter Operation");
+
+let object = { 
+    "+": "-",
+    "*": "+",
+    "-": "/",
+    "/": "**",
+};
+
+if (random > 0.1) {
+    // Perform correct calculation
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`);  //eval(2 - 3)
+} else {
+    // Perform wrong calculation
+    c = object[c];
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`);    
 }
