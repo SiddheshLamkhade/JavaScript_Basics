70f4e80cd891add8f3a8217d6aa438c0955af1a4
sid <sidlamkhade@gmail.com>
2024-09-21 19:36:45 +0530

    add

--- javaScript_Basics_Learning.js
+++ javaScript_Basics_Learning.js
@@ -7,7 +7,7 @@ g = 'amar';  // Dynamic language
 console.log(g);
 
 // Variables are declared using let keyword
-
+/*.............cut this line✂️✂️✂️✂️✂️✂️✂️✂️
 // ............🔴🟠🟡JavaScript Projects Faulty Calculator..........
 let random = Math.random();
 let a = prompt("Enter first number");
@@ -29,3 +29,59 @@ if (random > 0.1) {
     c = object[c];
     alert(`The result is ${eval(`${a} ${c} ${b}`)}`);    
 }
+...............cut this line✂️✂️✂️✂️✂️✂️✂️✂️*/
+
+//............🔴🟠🟡JavaScript Projects Buisenes Name Generator..........
+let rand=Math.random();
+let first , second, third;
+//0 0.33 0.66 100
+//let's generate first word
+if (rand<0.33)
+{
+    first="Crazy";
+}
+else if(rand<0.66 && rand>=0.33)
+{
+    first="Amazing";
+}
+else
+{
+    first="Fire";
+}
+
+
+//let's generate second word
+    rand=Math.random();
+    if (rand<0.33)
+    {
+        second="Engine";
+    }
+    else if(rand<0.66 && rand>=0.33)
+    {
+        second="Foods";
+    }
+    else
+    {
+        second="Garments";
+    }
+
+
+//let's generate third word
+    rand=Math.random();
+    if (rand<0.33)
+    {
+        third="Bros";
+    }
+    else if(rand<0.66 && rand>=0.33)
+    {
+        third="Limited";
+    }
+    else
+    {
+        third="Hub";
+    }
+
+    console.log(`${first} ${second} ${third}`);
+
+    //............🔴🟠🟡
+    
